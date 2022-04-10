e29833ac34966b5455d4861bc3ec3c4ccacd1760
LeonardoLordelloFontes <leonardolordellofontes@gmail.com>
2022-04-10 01:46:28 +0100

    adicionei a estrutura e as funcionalidades básicas da casa inteligente

--- src/test/java/com/grupoxx/smarthouse/SmartHouseTest.java
+++ src/test/java/com/grupoxx/smarthouse/SmartHouseTest.java
@@ -1,2 +1,26 @@
 package com.grupoxx.smarthouse;
 
+import com.grupoxx.smartdevice.SmartDevice;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmartHouseTest {
+    @Test
+    void itShouldAddSmartDeviceToHouse() {
+        // TODO
+    }
+
+    @Test
+    void itShouldAddSmartDeviceToRoom() {
+        // TODO
+    }
+
+    @Test
+    void itShouldAddRoom() {
+        // TODO
+    }
+
+    // TODO
+
+}
