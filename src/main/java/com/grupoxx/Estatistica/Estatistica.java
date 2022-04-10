db1caf0629a943bd62fabf1581d8a066667ab834
AdrianoFeixa <tiagoadriano.feixamoreira@gmail.com>
2022-04-10 21:40:07 +0100

    Escrevi um novo pacage e escrevi algo que vai ser util

--- src/main/java/com/grupoxx/Estatistica/Estatistica.java
+++ src/main/java/com/grupoxx/Estatistica/Estatistica.java
@@ -1,4 +1,11 @@
 package com.grupoxx.Estatistica;
 
+import com.grupoxx.EnergySupplier.EnergySupplier;
+import com.grupoxx.smarthouse.SmartHouse;
+
+import java.util.Set;
+
 public class Estatistica {
+    private Set <SmartHouse> house;
+    private EnergySupplier edp;
 }
