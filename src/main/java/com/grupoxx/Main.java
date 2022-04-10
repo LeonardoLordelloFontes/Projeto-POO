de12794aa3d21526743e38c68a2bc44191975f51
zeisalone <jmbarbosa2002@gmail.com>
2022-04-10 11:21:41 +0100

    Updates

--- src/main/java/com/grupoxx/Main.java
+++ src/main/java/com/grupoxx/Main.java
@@ -1,20 +1,8 @@
 package com.grupoxx;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+
 
 public class Main {
 
-        public static void main(String[] args) throws Exception {
-            ScriptEngineManager factory = new ScriptEngineManager();
-            ScriptEngine engine = factory.getEngineByName("Java");
-
-            engine.eval("value = 10");
-            Boolean greaterThan5 = (Boolean) engine.eval("value > 5");
-            Boolean lessThan5 = (Boolean) engine.eval("value < 5");
-
-            System.out.println(greaterThan5); // true
-            System.out.println(lessThan5); // false
-        }
 
 }
 
