0d768a9cfd453d3a7fcd196c9387d8eded8d122f
AdrianoFeixa <tiagoadriano.feixamoreira@gmail.com>
2022-04-13 13:25:50 +0100

    Adicionei metodos dentro da estatistica

--- src/main/java/com/grupoxx/menu/Menu.java
+++ src/main/java/com/grupoxx/menu/Menu.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Menu {
+
     public static int MenuInicial() {
         clearWindow();
         StringBuilder sb = new StringBuilder("-----------MENU INICIAL-----------\n\n");
--- src/main/java/com/grupoxx/menu/MenuController.java
+++ src/main/java/com/grupoxx/menu/MenuController.java
@@ -1,4 +1,5 @@
 package com.grupoxx.menu;
 
 public class MenuController {
+
 }
