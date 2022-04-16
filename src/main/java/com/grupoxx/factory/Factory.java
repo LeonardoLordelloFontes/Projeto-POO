8584df6df9a9175ace881c206eab09aaa07835b7
LeonardoLordelloFontes <leonardolordellofontes@gmail.com>
2022-04-16 13:42:27 +0100

    Adicionei a fabrica e alguns menus da smarthouse

---
+++ src/main/java/com/grupoxx/factory/Factory.java
@@ -0,0 +1,11 @@
+package com.grupoxx.factory;
+
+import com.grupoxx.smartdevice.SmartDeviceRepository;
+
+import java.util.Map;
+
+public class Factory {
+    SmartDeviceRepository smartDeviceRepository;
+    Map<String, Boolean> available;
+    String address;
+}
--- src/main/java/com/grupoxx/smarthouse/SmartHouseMenu.java
+++ src/main/java/com/grupoxx/smarthouse/SmartHouseMenu.java
@@ -1,20 +1,31 @@
 package com.grupoxx.smarthouse;
 
+import jdk.jshell.Snippet;
+
 import java.util.List;
 import java.util.Scanner;
 
 public class SmartHouseMenu {
+
+    /**
+     *
+     * @return
+     */
+
     public static int smartHouseMenu() {
         StringBuilder sb = new StringBuilder("-----------Casa-----------\n\n");
-        sb.append("1. Adicionar \n");
-        sb.append("2. Remover \n");
-        sb.append("3. Atualizar \n");
-        sb.append("4. Listar \n");
-        sb.append("5. Voltar \n\n");
+        sb.append("1. Adicionar Casa\n");
+        sb.append("2. Remover Casa\n");
+        sb.append("3. Atualizar dados da casa\n");
+        sb.append("4. Listar Casas \n");
+        sb.append("5. Ligar/Desligar dispositivos \n\n");
+        sb.append("6. Voltar \n\n");
         sb.append("Sua Opção (Selecionar Número): ");
         System.out.print(sb.toString());
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        int option = scanner.nextInt();
+        if (option < 1 || option > 6) return -1;
+        return option;
     }
 
     public static String[] smartHouseAddMenu() {
@@ -52,6 +63,10 @@ public class SmartHouseMenu {
         return input;
     }
 
+    /**
+     * Menu com opções para atualizar dados da casa
+     */
+
     public static int smartHouseUpdateMenu() {
         StringBuilder sb = new StringBuilder("-----------Atualizar Casa-----------\n\n");
         sb.append("1. Adicionar Divisão \n");
@@ -61,33 +76,71 @@ public class SmartHouseMenu {
         sb.append("5. Atualizar Endereço \n");
         sb.append("6. Atualizar Fornecedor de Energia \n");
         sb.append("7. Atualizar Proprietário \n");
-        sb.append("8. Atualizar dispositivo \n");
-        sb.append("9. Voltar \n");
+        sb.append("8. Voltar \n");
         sb.append("Sua Opção (Selecionar Número): ");
         System.out.print(sb.toString());
         Scanner scanner = new Scanner(System.in);
         int option = scanner.nextInt();
+        if (option < 1 || option > 8) return -1;
         return option;
     }
-    public static String smartHouseAddRoamMenu() {
-        StringBuilder sb = new StringBuilder("-----------Adicionar Divisão na Casa-----------\n\n");
-        sb.append("Nome da divisão (Para cancelar digite *): ");
-        System.out.println(sb.toString());
+
+    /**
+     * Menu que pode ser utilizado sempre que precisarmos pedir um endereço de uma casa para o usuário
+     * @return O endereço inserido pelo usuário, ou null, caso o usuário deseje cancelar a operação de escrita
+     */
+    public static String smartHouseUpdateAddressMenu() {
+        System.out.print("Endereço (para cancelar digite *): ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        if (input.equals("*")) return null;
         return input;
     }
 
+    /**
+     * Menu que pode ser utilizado sempre que precisarmos pedir os dados de um proprietário, isto é, nome e nif
+     * @return Um array de strings, onde na primeira posição está o nome, e na segunda o nif inserido pelo usuário,
+     *         ou null, caso o usuário deseje cancelar a operação de escrita
+     */
+
     public static String[] smartHouseUpdateOwnerMenu() {
-        String input[] = new String[2];
+        String[] input = new String[2];
         Scanner scanner = new Scanner(System.in);
-        System.out.print("-----------Adicionar Divisão na Casa-----------\n\nNIF (para cancelar digite *): ");
-        input[0] = scanner.next();
+        System.out.print("Nome (para cancelar digite *): ");
+        input[0] = scanner.nextLine();
         if (input[0].equals("*")) return null;
         scanner.nextLine();
-        System.out.print("Nome (para cancelar digite *): ");
-        input[1] = scanner.nextLine();
+        System.out.print("NIF (para cancelar digite *): ");
+        input[1] = scanner.next();
         if (input[1].equals("*")) return null;
+        return input;
+    }
+
+    /**
+     * Menu que pode ser utilizado sempre que precisarmos pedir o nome da divisão de uma casa para o usuário
+     * @return O nome da divisão da casa inserido pelo usuário, ou null, caso o usuário deseje cancelar a operação
+     *         de escrita
+     */
+
+    public static String smartHouseUpdateRoomMenu() {
+        System.out.print("Nome da divisão (para cancelar digite *): ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input.equals("*")) return null;
+        return input;
+    }
+
+    /**
+     * Menu que pode ser utilizado sempre que precisarmos pedir o nome do fornecedor de energia da casa para o usuário
+     * @return O nome do fornecedor de energia inserido pelo usuário, ou null, caso o usuário deseje cancelar
+     *         a operação
+     */
+
+    public static String smartHouseUpdateEnergySupplierMenu() {
+        System.out.println("Nome do fornecedor de energia (para cancelar digite *): ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input.equals("*")) return null;
         return input;
     }
 }
