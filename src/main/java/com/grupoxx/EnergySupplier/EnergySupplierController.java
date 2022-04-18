e5957b96eabd73c6fb7e023578dbe8b114f86d55
zeisalone <jmbarbosa2002@gmail.com>
2022-04-18 12:27:51 +0100

    Novas cenas

--- src/main/java/com/grupoxx/EnergySupplier/EnergySupplierController.java
+++ src/main/java/com/grupoxx/EnergySupplier/EnergySupplierController.java
@@ -34,14 +34,24 @@ public class EnergySupplierController {
         this.energySupplierRepository.addEnergySupplier(input[0], input[1]);
         energySupplierController();
     }
-    public void updateEnergySupplierController() {
+    public void updateEnergySupplierNameController() {
         String[] input = MainMenu.AtualizarFornecedordeEnergia();
         if(input.equals(null)) energySupplierController();
 
-        // this.energySupplierRepository.updateEnergySupplier(input[0],input[1],input[2],input[3]);
+        // this.energySupplierRepository.updateEnergySupplierName(input[0],input[1]);
+        energySupplierController();
+    }
+    public void updateEnergySupplierFormulaController() {
+        String[] input = MainMenu.AtualizarFornecedordeEnergia();
+        if(input.equals(null)) energySupplierController();
+
+        // this.energySupplierRepository.updateEnergySupplierFormula(input[0],input[1]);
         energySupplierController();
     }
     public void removeEnergySupplierController() {
-        //Todo
+        String[] input = MainMenu.AtualizarFornecedordeEnergia();
+        if(input.equals(null)) energySupplierController();
+
+        this.energySupplierRepository.removeEnergySupplier(input[0],input[1];
     }*/
 }
