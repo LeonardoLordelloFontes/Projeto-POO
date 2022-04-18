f503e0b99e2318e7612e3359584011ddb3c77a7e
LeonardoLordelloFontes <leonardolordellofontes@gmail.com>
2022-04-18 16:19:15 +0100

    energySupplierMenu

--- src/main/java/com/grupoxx/EnergySupplier/EnergySupplierMenu.java
+++ src/main/java/com/grupoxx/EnergySupplier/EnergySupplierMenu.java
@@ -7,14 +7,17 @@ public class EnergySupplierMenu {
 
     public static String smartHouseSelectEnergySupplierMenu(EnergySupplierRepository energySupplierRepository) {
         List<EnergySupplier> energySuppliers = energySupplierRepository.findAllEnergySuppliers();
+        if (energySuppliers.size() == 0) {
+            System.out.println("Não há fornecedores de energia disponíveis");
+            return null;
+        }
         StringBuilder sb = new StringBuilder("-----------Selecionar Fornecedor de Energia-----------\n\n");
         energySuppliers.forEach(energySupplier -> sb.append(energySupplier).append("\n"));
-        sb.append("Para cancelar a ação digite *\n");
-        sb.append("Selecione o fornecedor de energia (pelo nome): ");
+        sb.append("Selecione o fornecedor de energia pelo nome (para cancelar digite *): ");
         System.out.println(sb);
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        scanner.close();
+        if (input.equals("*")) return null;
         return input;
     }
 }
