24f51993a40b431e37ce48a3ecf495b32aa7ffd8
AdrianoFeixa <tiagoadriano.feixamoreira@gmail.com>
2022-05-11 23:21:05 +0100

    Toda a minha documentação feita

--- src/main/java/com/grupo11/community/Community.java
+++ src/main/java/com/grupo11/community/Community.java
@@ -5,6 +5,7 @@ import com.grupo11.factory.FactoryRepository;
 import com.grupo11.smarthouse.SmartHouseRepository;
 
 public class Community {
+
     private FactoryRepository factory;
     private EnergySupplierRepository energySuppliers;
     private SmartHouseRepository smartHouses;
@@ -12,6 +13,13 @@ public class Community {
     private Community() {
     }
 
+    /**
+     * Cronstrutor da comunidade
+     *
+     * @param factory o model factory
+     * @param energySuppliers o model energySuppliers
+     * @param smartHouses o model smartHouses
+     */
     public Community(FactoryRepository factory,
                      EnergySupplierRepository energySuppliers,
                      SmartHouseRepository smartHouses) {
@@ -20,14 +28,29 @@ public class Community {
         this.smartHouses = smartHouses;
     }
 
+    /**
+     * Metodo que partilha o apontador para o model FactoryRepository
+     *
+     * @return o apontador para o model FactoryRepository
+     */
     public FactoryRepository getFactory() {
         return factory;
     }
 
+    /**
+     * Metodo que partilha o apontador para o model EnergySuppliers
+     *
+     * @return o apontador para o model EnergySuppliers
+     */
     public EnergySupplierRepository getEnergySuppliers() {
         return energySuppliers;
     }
 
+    /**
+     *  Metodo que partilha o apontador para o model SmartHouses
+     *
+     * @return o apontador para o model SmartHouses
+     */
     public SmartHouseRepository getSmartHouses() {
         return smartHouses;
     }
