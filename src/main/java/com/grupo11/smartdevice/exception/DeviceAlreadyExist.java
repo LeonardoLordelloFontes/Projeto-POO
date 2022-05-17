629b5789ce018818fcfabf9b31add749bbdfa37b
AdrianoFeixa <tiagoadriano.feixamoreira@gmail.com>
2022-05-17 08:44:28 +0100

    Toda a minha documentação feita

--- src/main/java/com/grupo11/smartdevice/exception/DeviceAlreadyExist.java
+++ src/main/java/com/grupo11/smartdevice/exception/DeviceAlreadyExist.java
@@ -1,7 +1,14 @@
 package com.grupo11.smartdevice.exception;
 
+/**
+ * Exceção RuntimeException particular que ocorre quando queremos criar novos dispositivos
+ */
 public class DeviceAlreadyExist extends RuntimeException {
     public DeviceAlreadyExist(){super();}
 
+    /**
+     * Exceção se os dispositivos já existirem
+     * @param message é a mensagem que o utilizador lê quando os dispositivos já existem
+     */
     public DeviceAlreadyExist(String message){super(message);}
 }
--- src/main/java/com/grupo11/smartdevice/exception/DeviceNotFound.java
+++ src/main/java/com/grupo11/smartdevice/exception/DeviceNotFound.java
@@ -1,8 +1,15 @@
 package com.grupo11.smartdevice.exception;
 
+/**
+ * Exceção RuntimeException particular que ocorre quando queremos remover ou atualizar dispositivos
+ */
 public class DeviceNotFound extends RuntimeException{
 
     public DeviceNotFound(){super();}
 
+    /**
+     * Exceção se os dispositivos não existirem
+     * @param message é a mensagem que o utilizador lê quando os dispositivos não existem
+     */
     public DeviceNotFound(String message){super(message);}
 }
